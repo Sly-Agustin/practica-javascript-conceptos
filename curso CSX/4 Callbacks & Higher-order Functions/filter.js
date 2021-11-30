2c4efbfb6424324d906ee6ac24ffc3e26bf88407
Agustin Sly <agustin_sly@hotmail.com>
2021-11-30 16:54:47 -0300

    Update al modulo 4

--- curso CSX/4 Callbacks & Higher-order Functions/filter.js
+++ curso CSX/4 Callbacks & Higher-order Functions/filter.js
@@ -17,3 +17,29 @@ const over100 = n => n > 100;
 console.log(eitherFilter(arrOfNums, integerSquareRoot, over100)); // should log: [105, 9]
 
 // -----------------------------------------------------------------------------------------------------------------------------
+
+/* The result of using eitherCallback to combine two callbacks into one callback and then passing that one callback into filterArray 
+ should match the results of simply passing the two callbacks into eitherFilter in the previous challenge. */
+
+ function eitherCallback(callback1, callback2) {
+    return function(num){
+      return (callback1(num) || callback2(num));
+}
+}
+
+function filterArray(array, callback) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i += 1) {
+        if (callback(array[i], i, array)) newArray.push(array[i]);
+    }
+    return newArray;
+}
+const arrOfNums = [10, 35, 105, 9];
+const integerSquareRoot = n => Math.sqrt(n) % 1 === 0;
+const over100 = n => n > 100;
+const intSqRtOrOver100 = eitherCallback(integerSquareRoot, over100);
+console.log(filterArray(arrOfNums, intSqRtOrOver100)); // should log: [105, 9]
+
+ // -----------------------------------------------------------------------------------------------------------------------------
+
+ 
