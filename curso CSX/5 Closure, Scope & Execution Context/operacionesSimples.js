622dc4b80b87ee4b98c87a45be0557ab89f9d33c
Agustin Sly <agustin_sly@hotmail.com>
2021-12-14 16:36:04 -0300

    Update operacionesSimples.js

--- curso CSX/5 Closure, Scope & Execution Context/operacionesSimples.js
+++ curso CSX/5 Closure, Scope & Execution Context/operacionesSimples.js
@@ -122,3 +122,31 @@ const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
 console.log(multBy2AndLog(2)); // should log: 4
 console.log(multBy2AndLog(9)); // should log: 18
 console.log(multBy2AndLog('boo')); // should log: { 2: 4, 9: 18 }
+
+// -----------------------------------------------------------------------------------------------------------------------------
+
+/* Create a function cycleIterator that accepts an array, and returns a function. The returned function will accept zero arguments. 
+When first invoked, the returned function will return the first element of the array. When invoked a second time, the returned 
+function will return the second element of the array, and so forth. After returning the last element of the array, the next invocation 
+will return the first element of the array again, and continue on with the second after that, and so forth.*/
+
+function cycleIterator(arr){
+    let cycles=0;
+    return function() {
+        if(cycles==arr.length) {
+            cycles=0;
+      }
+        cycles++;
+        return arr[cycles-1];
+  }
+}
+
+const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
+const getDay = cycleIterator(threeDayWeekend);
+console.log(getDay()); // should log: 'Fri'
+console.log(getDay()); // should log: 'Sat'
+console.log(getDay()); // should log: 'Sun'
+console.log(getDay()); // should log: 'Fri'
+
+// -----------------------------------------------------------------------------------------------------------------------------
+
